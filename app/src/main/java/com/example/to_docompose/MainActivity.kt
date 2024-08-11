f6efe3b47f22bf462fed758be4f452ceada5d7c7
Uladzislau Melikhau <vladislavmelikhov@gmail.com>
2024-08-11 16:08:19 +0200

    TDC-1: Create a new project

---
+++ app/src/main/java/com/example/to_docompose/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.to_docompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.example.to_docompose.ui.theme.ToDoComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ToDoComposeTheme {
+
+            }
+        }
+    }
+}
