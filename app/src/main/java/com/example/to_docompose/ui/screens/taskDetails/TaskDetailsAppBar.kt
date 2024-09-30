b841370ff59e31ae5b1257d876f9223b6e1eba29
Uladzislau Melikhau <vladislavmelikhov@gmail.com>
2024-09-30 22:32:42 +0200

    TDC-31: Task Screen - Existing Task App Bar

--- app/src/main/java/com/example/to_docompose/ui/screens/taskDetails/TaskDetailsAppBar.kt
+++ app/src/main/java/com/example/to_docompose/ui/screens/taskDetails/TaskDetailsAppBar.kt
@@ -3,6 +3,8 @@ package com.example.to_docompose.ui.screens.taskDetails
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -11,8 +13,11 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.to_docompose.R
+import com.example.to_docompose.data.models.Priority
+import com.example.to_docompose.data.models.ToDoTask
 import com.example.to_docompose.navigation.ToDoTaskAction
 import com.example.to_docompose.ui.theme.LocalCustomColorsPalette
 import com.example.to_docompose.ui.theme.ToDoComposeTheme
@@ -34,7 +39,7 @@ fun NewTaskAppBar(
     TopAppBar(
         navigationIcon = {
             BackAction(
-                onBackClick = { navigateToTasksList(ToDoTaskAction.NO_ACTION) },
+                onClick = { navigateToTasksList(ToDoTaskAction.NO_ACTION) },
             )
         },
         title = {
@@ -48,7 +53,7 @@ fun NewTaskAppBar(
         ),
         actions = {
             AddAction(
-                onAddClick = { navigateToTasksList(ToDoTaskAction.ADD) },
+                onClick = { navigateToTasksList(ToDoTaskAction.ADD) },
             )
         },
     )
@@ -56,10 +61,10 @@ fun NewTaskAppBar(
 
 @Composable
 fun BackAction(
-    onBackClick: () -> Unit,
+    onClick: () -> Unit,
 ) {
     IconButton(
-        onClick = onBackClick,
+        onClick = onClick,
     ) {
         Icon(
             imageVector = Icons.AutoMirrored.Filled.ArrowBack,
@@ -71,14 +76,93 @@ fun BackAction(
 
 @Composable
 fun AddAction(
-    onAddClick: () -> Unit,
+    onClick: () -> Unit,
 ) {
     IconButton(
-        onClick = onAddClick,
+        onClick = onClick,
     ) {
         Icon(
             imageVector = Icons.Filled.Check,
             contentDescription = stringResource(R.string.add_task),
+            tint = LocalCustomColorsPalette.current.topAppBarContentColor,
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ExistingTaskAppBar(
+    task: ToDoTask,
+    navigateToTasksList: (ToDoTaskAction) -> Unit,
+) {
+    TopAppBar(
+        navigationIcon = {
+            CloseAction(
+                onClick = { navigateToTasksList(ToDoTaskAction.NO_ACTION) },
+            )
+        },
+        title = {
+            Text(
+                text = task.title,
+                color = LocalCustomColorsPalette.current.topAppBarContentColor,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+            )
+        },
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = LocalCustomColorsPalette.current.topAppBarBackgroundColor,
+        ),
+        actions = {
+            DeleteAction(
+                onClick = { navigateToTasksList(ToDoTaskAction.DELETE) }
+            )
+            UpdateAction(
+                onClick = { navigateToTasksList(ToDoTaskAction.UPDATE) }
+            )
+        },
+    )
+}
+
+@Composable
+fun CloseAction(
+    onClick: () -> Unit,
+) {
+    IconButton(
+        onClick = onClick,
+    ) {
+        Icon(
+            imageVector = Icons.Filled.Close,
+            contentDescription = stringResource(R.string.close_icon),
+            tint = LocalCustomColorsPalette.current.topAppBarContentColor,
+        )
+    }
+}
+
+@Composable
+fun DeleteAction(
+    onClick: () -> Unit,
+) {
+    IconButton(
+        onClick = onClick,
+    ) {
+        Icon(
+            imageVector = Icons.Filled.Delete,
+            contentDescription = stringResource(R.string.delete_icon),
+            tint = LocalCustomColorsPalette.current.topAppBarContentColor,
+        )
+    }
+}
+
+@Composable
+fun UpdateAction(
+    onClick: () -> Unit,
+) {
+    IconButton(
+        onClick = onClick,
+    ) {
+        Icon(
+            imageVector = Icons.Filled.Check,
+            contentDescription = stringResource(R.string.update_icon),
             tint = LocalCustomColorsPalette.current.topAppBarContentColor,
         )
     }
@@ -89,6 +173,22 @@ fun AddAction(
 fun NewTaskAppBarPreview() {
     ToDoComposeTheme(darkTheme = false) {
         NewTaskAppBar(
+            navigateToTasksList = {},
+        )
+    }
+}
+
+@Composable
+@Preview
+fun ExistingTaskAppBarPreview() {
+    ToDoComposeTheme(darkTheme = false) {
+        ExistingTaskAppBar(
+            task = ToDoTask(
+                id = 123,
+                title = "Long title of some task from the list",
+                description = "Task description",
+                priority = Priority.MEDIUM,
+            ),
             navigateToTasksList = {},
         )
     }
